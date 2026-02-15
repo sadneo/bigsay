d90fa097afba4d4c72cae38a0fa5379122c9040c
sadneo <haywardeel@gmail.com>
2026-02-14 22:18:05 -0500

    feat: it works now

--- bigsay.py
+++ bigsay.py
@@ -1,6 +1,6 @@
 import gi
-from gi.repository import Gtk, Gdk, Pango, PangoCairo
 gi.require_version("Gtk", "4.0")
+from gi.repository import Gtk, Gdk, Pango, PangoCairo
 
 class BigSayStyled(Gtk.Window):
     def __init__(self, application):
--- flake.nix
+++ flake.nix
@@ -1,26 +1,76 @@
 {
-  description = "Python development shell";
-
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
   outputs =
-    { self, nixpkgs }:
     {
-      devShells.x86_64-linux.default =
-        let
-          pkgs = import nixpkgs {
-            system = "x86_64-linux";
-          };
-        in
-        pkgs.mkShell {
-          buildInputs = with pkgs; [
-            (pkgs.python313.withPackages (python-pkgs: [
-              python-pkgs.pygobject3
-            ]))
-            wrapGAppsHook
-            gobject-introspection
-            gtk4
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # System dependencies required for GObject/GTK at runtime
+        runtimeDeps = with pkgs; [
+          gtk4
+          pango
+          gdk-pixbuf
+          gobject-introspection
+        ];
+
+        # Build-time dependencies
+        buildDeps = with pkgs; [
+          pkg-config
+          gobject-introspection
+        ];
+      in
+      {
+        # The actual package build
+        packages.default = pkgs.python3Packages.buildPythonApplication {
+          pname = "bigsay";
+          version = "0.1.0";
+          pyproject = true;
+          src = ./.;
+
+          nativeBuildInputs = [ pkgs.wrapGAppsHook4 ] ++ buildDeps;
+          buildInputs = runtimeDeps;
+
+          propagatedBuildInputs = with pkgs.python3Packages; [
+            pygobject3
           ];
         };
-    };
+
+        # The interactive development shell
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.uv
+            pkgs.ruff
+          ]
+          ++ runtimeDeps
+          ++ buildDeps;
+
+          shellHook = ''
+            # Create the virtualenv if it doesn't exist
+            if [ ! -d ".venv" ]; then
+              uv venv
+            fi
+
+            # Source the venv so 'python' and 'pip' point to it
+            source .venv/bin/activate
+
+            # Ensure uv uses the Nix-provided system libraries for builds
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeDeps}"
+            export GI_TYPELIB_PATH="${pkgs.lib.makeSearchPath "lib/girepository-1.0" runtimeDeps}"
+
+            echo "BigSay dev environment loaded with 'uv'"
+            fish
+          '';
+        };
+      }
+    );
 }
